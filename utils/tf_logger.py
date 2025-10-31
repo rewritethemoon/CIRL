9bb0137888024b75b6584424c84b3be6684cb079
Trần Gia Huy <111002872+rewritethemoon@users.noreply.github.com>
2025-10-31 22:39:05 +0700

    Update tf_logger.py

--- utils/tf_logger.py
+++ utils/tf_logger.py
@@ -1,5 +1,6 @@
 # Code referenced from https://gist.github.com/gyglim/1f8dfb1b5c82627ae3efcfbbadb9f514
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 import numpy as np
 import scipy.misc
 
